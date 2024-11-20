5c88f33a2d11818e7d87960108d176bb4c982ad7
utkarsh <123744256+um26@users.noreply.github.com>
2024-11-20 23:41:55 +0530

    Create sum_under_modulo.java

---
+++ sum_under_modulo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class sum_under_modulo{
+  public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    int a = scan.nextInt();
+	    int b = scan.nextInt();
+	    int c = scan.nextInt();
+	    System.out.println((a+b)%c);
+	}
+}
