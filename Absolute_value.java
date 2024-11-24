430133b958f13d4df01ef0b64714e7ae96e07ed4
utkarsh <123744256+um26@users.noreply.github.com>
2024-11-25 00:13:16 +0530

    Update Absolute_value.java

--- Absolute_value.java
+++ Absolute_value.java
@@ -1,15 +1,23 @@
 import java.util.Scanner;
 
-class Absolute_value{
-    public static void main(String[] args){
+class Absolute_value
+{
+    public static void main(String[] args)
+    {
 
     Scanner N = new Scanner(System.in);
     int n= N.nextInt();
-    if (n>=0){
+        
+    if (n>=0)
+    {
         System.out.println(n);
     }
-    else{
+        
+    else
+    {
         System.out.println(n*-1);
     }
-}
+        
+    }
+    
 }
