99271b49fcd7af3ca7e013110047d0d0f67803f3
utkarsh <123744256+um26@users.noreply.github.com>
2024-11-25 08:53:11 +0530

    Create n_sum.java

---
+++ n_sum.java
@@ -0,0 +1,12 @@
+//Write a program to find the sum of the given series 1+2+3+ . . . . . . (n terms)
+
+class Solution {
+    public static long seriesSum(int n) {
+        // code here
+        long sum =0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+}
